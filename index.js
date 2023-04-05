79164f7fd3c60376ccb4987e4502f3400b3ee825
justcallmeahann <80017838+justcallmeahann@users.noreply.github.com>
2023-04-06 00:47:38 +0700

    setup env, express, cors

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const masterRouter = require("./src/routes");
 
 const app = express();
 const port = process.env.APP_PORT || 3000;
 
 app.use(cors());
+
+// routers
+app.use(masterRouter);
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
