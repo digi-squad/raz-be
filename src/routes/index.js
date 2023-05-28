7e72572b5b24d5ea85cb95f5a8302b0c9c364a6f
Digital Squad <129971270+digi-squad@users.noreply.github.com>
2023-05-28 14:28:16 +0700

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,11 @@ const masterRouter = Router();
 
 masterRouter.use("/apiv1", apiv1Router);
 masterRouter.use("/products", productsRouter);
+masterRouter.get("/", (req, res) => {
+  res.status(200).json({
+  status: 200,
+  msg: "Welcome to RAZ Furniture Shop REST API!"}),
+});
 
 
 module.exports = masterRouter;
